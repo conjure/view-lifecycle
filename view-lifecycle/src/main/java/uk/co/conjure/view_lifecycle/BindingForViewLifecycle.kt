feda8da766a18f1455d9e13066a75ef92147cfa5
Simon Osim <simon.osim@osim.at>
2023-03-29 10:45:44 +0200

    Update kotlin doc

--- view-lifecycle/src/main/java/uk/co/conjure/view_lifecycle/BindingForViewLifecycle.kt
+++ view-lifecycle/src/main/java/uk/co/conjure/view_lifecycle/BindingForViewLifecycle.kt
@@ -7,45 +7,51 @@ import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
 /**
+ *
  * A function to be used with by delegation which will ensure that the fragments reference to its view
  * binding is held only as long as the view lifecycle is alive. If you neglect to clear reference to
  * the view binding when the fragment is moved to the background you get a potentially costly memory leak.
+ *
  * This function can be used to automate this reference management like so:
  *
- * class MyFragment : Fragment() {
- *      private var binding by bindingForViewLifecycle<MyFragmentBinding>()
  *
- *      override fun onCreateView(
- *          inflater: LayoutInflater,
- *          container: ViewGroup?,
- *          savedInstanceState: Bundle?
- *      ): View {
- *          binding = MyFragmentBinding.inflate(inflater, container, false)
- *          return binding.root
+ *      class MyFragment : Fragment() {
+ *          private var binding by bindingForViewLifecycle<MyFragmentBinding>()
+ *
+ *          override fun onCreateView(
+ *              inflater: LayoutInflater,
+ *              container: ViewGroup?,
+ *              savedInstanceState: Bundle?
+ *          ): View {
+ *              binding = MyFragmentBinding.inflate(inflater, container, false)
+ *              return binding.root
+ *          }
  *      }
- * }
+ *
  *
  * This simplifies the often suggested method of managing this memory leak by setting the binding
  * to null in onDestroyView like so:
  *
- * class MyFragment : Fragment() {
  *
- *      private var binding: MyFragmentBinding? = null
+ *      class MyFragment : Fragment() {
  *
- *      override fun onCreateView(
- *          inflater: LayoutInflater,
- *          container: ViewGroup?,
- *          savedInstanceState: Bundle?
- *      ): View {
- *          binding = MyFragmentBinding.inflate(inflater, container, false)
- *          return binding!!.root
+ *          private var binding: MyFragmentBinding? = null
+ *
+ *          override fun onCreateView(
+ *              inflater: LayoutInflater,
+ *              container: ViewGroup?,
+ *              savedInstanceState: Bundle?
+ *          ): View {
+ *              binding = MyFragmentBinding.inflate(inflater, container, false)
+ *              return binding!!.root
+ *          }
+ *
+ *          override fun onDestroyView() {
+ *              super.onDestroyView()
+ *              binding = null
+ *          }
  *      }
  *
- *      override fun onDestroyView() {
- *          super.onDestroyView()
- *          binding = null
- *      }
- * }
  *
  * This method is provided for convenience in situations where [LifecycleView] is overkill
  * but in general [LifecycleView] should be the preferred solution as it also helps to separate
--- view-lifecycle/src/main/java/uk/co/conjure/view_lifecycle/LifecycleView.kt
+++ view-lifecycle/src/main/java/uk/co/conjure/view_lifecycle/LifecycleView.kt
@@ -6,8 +6,47 @@ import androidx.lifecycle.LifecycleOwner
 import androidx.viewbinding.ViewBinding
 
 /**
- * Parent class for the View layer in an MVVM architecture.
- * It is a ViewBindingHolder (the View) that is also Lifecycle aware.
+ * A Lifecycle aware class to separate View logic from Fragment or Activity level concerns like navigation.
+ * Use [registerBinding] in an Activities onCreate() or Fragments onCreateView() to set the binding.
+ * Then implement the lifecycle methods [onCreate], [onStart], [onStop], [onDestroy] to handle the lifecycle events for the View.
+ *
+ *
+ * ### Code sample
+ * Example of a Fragment implementing a Counter
+ * ```
+ * class CounterFragment : Fragment() {
+ *
+ *	    private lateinit var counterView: CounterView
+ *
+ *	    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+ *	        counterView = CounterView()
+ *	        counterView.registerBinding(CounterViewBinding.inflate(inflater, container, false),this)
+ *	        return counterView.requireBinding().root
+ *	    }
+ *  }
+ * ```
+ * And the View implementation
+ *
+ * ```
+ * class CounterView : LifecycleView<CounterViewBinding>() {
+ *
+ *      var counter = 0
+ *
+ *	    override fun onCreate() {
+ *	        super.onCreate()
+ *
+ *	        binding.btnIncrease.setOnClickListener {
+ *		        counter++
+ *		        binding.tvCounterValue.text = counter.toString()
+ *	        }
+ *	    }
+ *	}
+ * ```
+ *
+ * This pattern can reduce the size of Fragments and Activities by moving View logic into a separate class.
+ * Activities and Fragments can then be used to handle navigation and other concerns.
+ *
+ * We recommend using ViewModels to handle the state of the View and communicate with the rest of the app.
  */
 abstract class LifecycleView<B : ViewBinding> : ViewBindingHolder<B>() {
 
@@ -32,6 +71,22 @@ abstract class LifecycleView<B : ViewBinding> : ViewBindingHolder<B>() {
                 }
             }
 
+            override fun onResume(owner: LifecycleOwner) {
+                called = false
+                onResume()
+                if (!called) {
+                    throw IllegalStateException("View $this did not call through to super.onStart()")
+                }
+            }
+
+            override fun onPause(owner: LifecycleOwner) {
+                called = false
+                onPause()
+                if (!called) {
+                    throw IllegalStateException("View $this did not call through to super.onStart()")
+                }
+            }
+
             override fun onStop(owner: LifecycleOwner) {
                 called = false
                 onStop()
@@ -51,21 +106,61 @@ abstract class LifecycleView<B : ViewBinding> : ViewBindingHolder<B>() {
         })
     }
 
+    /**
+     * Called when the view is created.
+     *
+     * Matches the [androidx.lifecycle.Lifecycle.Event.ON_CREATE] event.
+     */
     @CallSuper
     open fun onCreate() {
         called = true
     }
 
+    /**
+     * Called when the view is started.
+     *
+     * Matches the [androidx.lifecycle.Lifecycle.Event.ON_START] event.
+     */
     @CallSuper
     open fun onStart() {
         called = true
     }
 
+    /**
+     * Called when the view is resumed.
+     *
+     * Matches the [androidx.lifecycle.Lifecycle.Event.ON_RESUME] event.
+     */
+    @CallSuper
+    open fun onResume() {
+        called = true
+    }
+
+    /**
+     * Called when the view is paused.
+     *
+     * Matches the [androidx.lifecycle.Lifecycle.Event.ON_PAUSE] event.
+     */
+    @CallSuper
+    open fun onPause() {
+        called = true
+    }
+
+    /**
+     * Called when the view is stopped.
+     *
+     * Matches the [androidx.lifecycle.Lifecycle.Event.ON_STOP] event.
+     */
     @CallSuper
     open fun onStop() {
         called = true
     }
 
+    /**
+     * Called when the view is destroyed.
+     *
+     * Matches the [androidx.lifecycle.Lifecycle.Event.ON_DESTROY] event.
+     */
     @CallSuper
     open fun onDestroy() {
         called = true
